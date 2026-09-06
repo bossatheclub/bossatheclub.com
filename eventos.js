42558fc1432698636ede1fb6574597314423d6f3
bossatheclub <hectorpericet92@gmail.com>
2026-09-06 19:32:09 +0200

    Create eventos.js

---
+++ eventos.js
@@ -0,0 +1,35 @@
+window.BOSSA_EVENTOS = [
+
+  {
+    fecha: "Dom 04 · Oct 2026 · 18:00h",
+    nombre: "BOSSA TARDEO",
+    sala: "El Sótano · Madrid",
+    cartel: "TBA.png",
+    link: "https://es.ra.co/promoters/177842"
+  },
+
+  {
+    fecha: "Dom 11 · Oct 2026 · 18:00h",
+    nombre: "BOSSA TARDEO",
+    sala: "El Sótano · Madrid",
+    cartel: "TBA.png",
+    link: "https://es.ra.co/promoters/177842"
+  },
+
+  {
+    fecha: "Dom 18 · Oct 2026 · 18:00h",
+    nombre: "BOSSA TARDEO",
+    sala: "El Sótano · Madrid",
+    cartel: "TBA.png",
+    link: "https://es.ra.co/promoters/177842"
+  },
+
+  {
+    fecha: "Sab 24 · Oct 2026 · 00:00h",
+    nombre: "BOSSA",
+    sala: "TBA · Madrid",
+    cartel: "TBA.png",
+    link: "https://es.ra.co/promoters/177842"
+  }
+
+];
